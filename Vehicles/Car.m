6f75b04f0dab7bf0b2dcd6870cc7864f2c9512f3
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-04-14 11:00:12 -0700

    followed up till Inhertiance

---
+++ Vehicles/Car.m
@@ -0,0 +1,49 @@
+//
+//  Car.m
+//  Vehicles
+//
+//  Created by Brian Vo on 2018-04-14.
+//  Copyright © 2018 Designated Nerd Software. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.numberOfWheels = 4;
+    }
+    return self;
+}
+
+#pragma mark - Private method implementations
+- (NSString *)start
+{
+    return [NSString stringWithFormat:@"Start power source %@.", self.powerSource];
+}
+
+#pragma mark - Superclass Overrides
+- (NSString *)goForward
+{
+    return [NSString stringWithFormat:@"%@ %@ Then depress gas pedal.", [self start], [self changeGears:@"Forward"]];
+}
+
+- (NSString *)goBackward
+{
+    return [NSString stringWithFormat:@"%@ %@ Check your rear view mirror. Then depress gas pedal.", [self start], [self changeGears:@"Reverse"]];
+}
+
+- (NSString *)stopMoving
+{
+    return [NSString stringWithFormat:@"Depress brake pedal. %@", [self changeGears:@"Park"]];
+}
+
+- (NSString *)makeNoise
+{
+    return @"Beep beep!";
+}
+
+@end
